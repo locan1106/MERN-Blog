4605a5178df3027761840df28fcdfd832bdadf3b
locan1106 <phamlocan1106@gmail.com>
2024-02-21 18:31:10 +0700

    install react-router and create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
 const App = () => {
-	return <h1 className="text-black bg-red-500">App</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
