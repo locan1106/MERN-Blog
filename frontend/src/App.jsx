8dee249ee3b8b22b51f452174ce8355bc2eeb773
locan1106 <phamlocan1106@gmail.com>
2024-02-22 21:51:06 +0700

    create footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
 	return (
@@ -19,6 +20,7 @@ const App = () => {
 				<Route path="/projects" element={<Projects />} />
 				<Route path="/dashboard" element={<Dashboard />} />
 			</Routes>
+			<Footer />
 		</BrowserRouter>
 	);
 };
