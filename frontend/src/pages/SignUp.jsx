5db11ce62f7871c886fccbe1173de689cecaecda
locan1106 <phamlocan1106@gmail.com>
2024-02-23 10:01:33 +0700

    fix setLoading response false

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -29,9 +29,9 @@ const SignUp = () => {
 			const data = await res.json();
 
 			if (data.success === false) {
+				setLoading(false);
 				return setErrorMessage(data.message);
 			}
-			setLoading(false);
 			if (res.ok) {
 				navigate("/sign-in");
 			}
