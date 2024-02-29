40d361e307520cb64d2c76a3d264d0612281aba1
locan1106 <phamlocan1106@gmail.com>
2024-02-29 16:23:20 +0700

    delete console.log in createpost.jsx

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -92,8 +92,6 @@ const CreatePost = () => {
 		}
 	};
 
-	console.log(formData);
-
 	return (
 		<div className="p-3 max-w-3xl mx-auto min-h-screen">
 			<h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
