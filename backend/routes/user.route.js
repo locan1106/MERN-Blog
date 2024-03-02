e3cf812918fbcfeb91c1048b8d06ecb200eecad0
locan1106 <phamlocan1106@gmail.com>
2024-03-02 10:49:23 +0700

    create get users api route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
 	deleteUser,
 	getUser,
+	getUsers,
 	signOut,
 	updateUser,
 } from "../controllers/user.controller.js";
@@ -13,5 +14,6 @@ router.get("/get-all", getUser);
 router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/sign-out", signOut);
+router.get("/get-users", verifyToken, getUsers);
 
 export default router;
