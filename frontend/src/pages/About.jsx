82ca89e0913023038e0d0bc3ffc76da3e460a40c
locan1106 <phamlocan1106@gmail.com>
2024-03-07 10:32:45 +0700

    add about and projects page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,34 @@
 const About = () => {
-	return <div>About</div>;
+	return (
+		<div className="min-h-screen flex items-center justify-center">
+			<div className="max-w-2xl mx-auto p-3 text-center">
+				<div>
+					<h1 className="text-3xl font font-semibold text-center my-7">
+						{/* eslint-disable-next-line react/no-unescaped-entities */}
+						About LocAn's Blog
+					</h1>
+					<div className="text-md text-gray-500 flex flex-col gap-6">
+						<p>
+							{/* eslint-disable-next-line react/no-unescaped-entities */}
+							Welcome to LocAn's Blog!
+						</p>
+
+						<p>
+							{/* eslint-disable-next-line react/no-unescaped-entities */}
+							On this blog, you'll find weekly articles and tutorials on topics
+							such as web development, software engineering, and programming
+							languages.
+						</p>
+						<p>
+							We encourage you to leave comments on our posts and engage with
+							other readers. You can like other people&apos;s comments and reply
+							to them as well.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default About;
